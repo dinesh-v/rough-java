917f2ba3af3ee14d266e5e37d1dbaeb70d27ec4a
Dinesh V <dinesh-v@users.noreply.github.com>
2017-03-07 06:23:05 +0530

    Pass by value & Pass by reference
    Reference:
    http://stackoverflow.com/questions/12757841/are-arrays-passed-by-value-or-passed-by-reference-in-java
    http://stackoverflow.com/questions/3662288/can-i-pass-a-primitive-type-by-reference-in-java
    http://stackoverflow.com/documentation/java/4388/java-pitfalls/24130/pitfall-thinking-of-variables-as-objects#t=20161119160046255848

--- src/main/java/com/example/rough/App.java
+++ src/main/java/com/example/rough/App.java
@@ -1,9 +1,30 @@
 package com.example.rough;
 
-public class App
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        int x = 10;
+        add(x);
+        System.out.println(x);
+        Integer integer = 10;
+        add(integer);
+        System.out.println(integer);
+        int[] intArray = new int[1];
+        intArray[0] = 10;
+        add(intArray);
+        System.out.println(intArray[0]);
+
     }
+
+    private static void add(int x) {
+        x += 10;
+    }
+
+    private static void add(Integer x) {
+        x += 10;
+    }
+
+    private static void add(int... intArray) {
+        intArray[0] += 10;
+    }
+
 }
