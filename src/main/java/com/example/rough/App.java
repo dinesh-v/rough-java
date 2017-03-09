39572c19e569edf5add8e1020feef6e89c395004
Dinesh V <dinesh-v@users.noreply.github.com>
2017-03-09 17:46:12 +0530

    Make project to use Java 8 Implement Comparator in Java

--- src/main/java/com/example/rough/App.java
+++ src/main/java/com/example/rough/App.java
@@ -1,30 +1,90 @@
 package com.example.rough;
 
+import java.util.Comparator;
+import java.util.TreeSet;
+
 public class App {
     public static void main(String[] args) {
-        int x = 10;
-        add(x);
-        System.out.println(x);
-        Integer integer = 10;
-        add(integer);
-        System.out.println(integer);
-        int[] intArray = new int[1];
-        intArray[0] = 10;
-        add(intArray);
-        System.out.println(intArray[0]);
 
+        // http://stackoverflow.com/questions/6478515/return-type-from-a-comparator
+
+
+        // "d" character has larger value than "a" >1 will be returned. Here 3 is returned which is difference between a & d
+        System.out.println("dinesh".compareTo("air"));
+
+        // Equal will return 0
+        System.out.println("dinesh".compareTo("dinesh"));
+
+        // "a" character has larger value than "d" so <1 will be returned. Here -3 is returned which is difference between a & d
+        System.out.println("air".compareTo("dinesh"));
+
+        //By using name comparator (String comparison)
+        TreeSet<Employee> nameComp = new TreeSet<>(new MyNameComp());
+        nameComp.add(new Employee("Ram", 3000));
+        nameComp.add(new Employee("John", 6000));
+        nameComp.add(new Employee("Crish", 2000));
+        nameComp.add(new Employee("Tom", 2400));
+
+        nameComp.forEach(System.out::println);
+
+        System.out.println("\nPrinting based on salary from lowest to highest\n");
+
+        //By using salary comparator (int comparison)
+        TreeSet<Employee> salComp = new TreeSet<>(new MySalaryComp());
+        salComp.add(new Employee("Ram", 3000));
+        salComp.add(new Employee("John", 6000));
+        salComp.add(new Employee("Crish", 2000));
+        salComp.add(new Employee("Tom", 2400));
+        salComp.forEach(System.out::println);
+    }
+}
+
+class MyNameComp implements Comparator<Employee> {
+    @Override
+    public int compare(Employee e1, Employee e2) {
+        return e1.getName().compareTo(e2.getName());
+    }
+}
+
+class MySalaryComp implements Comparator<Employee> {
+    @Override
+    public int compare(Employee e1, Employee e2) {
+        if (e1.getSalary() > e2.getSalary()) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+}
+
+class Employee {
+
+    private String name;
+    private int salary;
+
+    Employee(String n, int s) {
+        this.name = n;
+        this.salary = s;
     }
 
-    private static void add(int x) {
-        x += 10;
+    String getName() {
+        return name;
     }
 
-    private static void add(Integer x) {
-        x += 10;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    private static void add(int... intArray) {
-        intArray[0] += 10;
+    int getSalary() {
+        return salary;
     }
 
+    void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + this.name + " - Salary: " + this.salary;
+    }
 }
